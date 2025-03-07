c4bcd22ce7e620d673722cf586ab0a4f9edde626
Luke Harris <6074660+luk707@users.noreply.github.com>
2025-03-07 16:07:57 +0000

    Implement file download on the frontend

--- client/app/api/files.ts
+++ client/app/api/files.ts
@@ -38,3 +38,17 @@ export async function removeFile(path: string): Promise<void> {
     throw new Error(`Failed to delete ${path}: ${response.statusText}`);
   }
 }
+
+export function downloadFile(path: string): Promise<void> {
+  return fetch(`http://127.0.0.1:8000/workspaces/files/download/${path}`)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      // TODO: When we implement multiple folders, we will need a way to
+      // know the basename of the file, and its path
+      link.download = path;
+      link.click();
+    })
+    .catch(console.error);
+}
