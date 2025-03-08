87068674c8fc10290ba9d47cd81c97ef29b3af8b
dan-m0 <daniel.mcclarence@Gmail.com>
2025-03-08 08:19:37 +0000

    feat: added basename, path, and is_dir to File class and @from_minio_object method's assignments
    Updated logic to use local var for obj.object_name, instead of repeating the call to obj.object_name

--- server/src/models/file.py
+++ server/src/models/file.py
@@ -1,3 +1,4 @@
+from os import name
 from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
 from datetime import datetime
@@ -9,16 +10,23 @@ class File(BaseModel):
     model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
 
     name: str
+    basename: str
+    path: str
     content_type: str = "application/octet-stream"
     size: int
     last_modified: datetime
+    is_dir: bool
 
     @staticmethod
     def from_minio_object(obj: Object) -> "File":
-        return File(
-            name=obj.object_name,
-            content_type=obj.content_type
-            or mimetypes.guess_file_type(obj.object_name)[0],
-            size=obj.size,
-            last_modified=obj.last_modified,
-        )
+      obj_name = obj.object_name
+      return File(
+          name=obj_name,
+          content_type=obj.content_type
+          or mimetypes.guess_file_type(obj.object_name)[0],
+          size=obj.size,
+          last_modified=obj.last_modified,
+          basename=obj_name.split("/")[-1].split(".")[0],
+          path="/".join(obj_name.split("/")[:-1]) if len(obj_name.split("/")) > 1 else "/",
+          is_dir=True if obj_name.endswith("/") else False
+          )
