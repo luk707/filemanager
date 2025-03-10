86413e0596fac223680f955633ef72e3263704b5
dan-m0 <daniel.mcclarence@Gmail.com>
2025-03-10 11:31:24 +0000

    feat: Refactor file status retrieval to use dependency injection
    implemented stat only so far

--- server/src/main.py
+++ server/src/main.py
@@ -12,7 +12,7 @@ from minio.deleteobjects import DeleteObject
 from minio.error import S3Error
 
 from src.clients.minio import client
-from src.models.file import DirectoryListing, directory_listing_from_object
+from src.models.file import DirectoryListing
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn.error")
@@ -83,46 +83,18 @@ async def ready():
     summary="List directory",
     description="Returns a list of files and directories in the specified directory.",
 )
-async def stat(workspace_id: str, path: Optional[str] = None) -> list[DirectoryListing]:
-    """
-    Asynchronously retrieves the status of files in a specified workspace.
-
-    Args:
-      workspace_id (str): The ID of the workspace.
-      path (Optional[str], optional): The path within the workspace. Defaults to "".
-
-    Returns:
-      list[File]: A list of File objects representing the files in the workspace.
-
-    Raises:
-      HTTPException: If the specified path is not found in the workspace.
-    """
-
+async def stat(
+    file_repository: FileRepositoryDependency,
+    workspace_id: str,
+    path: Optional[str] = None,
+) -> list[DirectoryListing]:
     try:
-        objects = list(
-            client.list_objects(
-                workspace_id, prefix=f"{path}/" if path is not None else None
-            )
-        )
-        return [
-            directory_listing_from_object(obj)
-            for obj in objects
-            if obj.object_name != f"{path}/"
-        ]
+        return await file_repository.stat(workspace_id, path)
     except S3Error:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"404_NOT_FOUND: {path} not found in {workspace_id}",
         )
-
-    # -- for a later date --
-    # TODO: as well a listing of the files in the directory, we should also list
-    #       the folders within the directory
-    # TODO: Return a union list when listing i.e list[File|Folder] to tell the
-    #       client what folders exist in the current path
-    # TODO: Use the path option to filter files in a specific path of the bucket
-
-    # TODO: Check user has read permission for workspace
 
 
 @app.get(
