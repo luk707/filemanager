ed084a93101d1f89b05dca439daae66deed216ec
Luke Harris <6074660+luk707@users.noreply.github.com>
2025-03-11 16:33:42 +0000

    Add unit tests

---
+++ server/tests/test_main.py
@@ -0,0 +1,14 @@
+import pytest
+from fastapi.testclient import TestClient
+from fastapi import status
+from src.main import app
+
+
+@pytest.fixture
+def test_client():
+    return TestClient(app)
+
+
+def test_ready(test_client):
+    response = test_client.get("/ready")
+    assert response.status_code == status.HTTP_200_OK
