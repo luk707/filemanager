820c8a938bb7752fadb56296f94852801668f91f
Luke Harris <6074660+luk707@users.noreply.github.com>
2025-03-12 15:33:40 +0000

    Add assertion for successful response

--- server/tests/test_main.py
+++ server/tests/test_main.py
@@ -5,6 +5,9 @@ from src.main import app
 from src.repositories.files.fastapi import get_file_repository
 from src.repositories.files.base import FileRepository
 from minio.error import S3Error
+from src.models.file import File, Directory
+from datetime import datetime
+from fastapi.encoders import jsonable_encoder
 
 
 @pytest.fixture
@@ -22,18 +25,31 @@ def test_stat_successful(mocker, test_client):
     mock_file_repository = mocker.MagicMock(
         spec=FileRepository,
     )
-    mock_file_repository.stat = mocker.AsyncMock(return_value=[])
+    mock_response = [
+        File(
+            name="some/path/test_file.txt",
+            basename="test_file.txt",
+            path="some/path",
+            content_type="text/plain",
+            size=100,
+            last_modified=datetime.now(),
+        ),
+        Directory(
+            name="some/path/subdir/",
+            path="some/path",
+        ),
+    ]
+    mock_file_repository.stat = mocker.AsyncMock(return_value=mock_response)
     app.dependency_overrides[get_file_repository] = lambda: mock_file_repository
-
     response = test_client.get(
         "/workspaces/test_workspace_id/stat/some/path",
     )
-
     mock_file_repository.stat.assert_called_once_with(
         "test_workspace_id",
         "some/path",
     )
     assert response.status_code == status.HTTP_200_OK
+    assert response.json() == jsonable_encoder(mock_response)
 
 
 def test_stat_failure(mocker, test_client):
