53971f46cafd47105876ee792e7d8e24895cc917
Luke Harris <6074660+luk707@users.noreply.github.com>
2025-03-13 11:49:48 +0000

    Configure storybook (#66)

---
+++ client/.storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-themes"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {
+      builder: {
+        viteConfigPath: "sb-vite.config.ts",
+      },
+    },
+  },
+};
+
+export default config;
