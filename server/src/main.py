54def5046877d0af2df97d0f6535f08ec7377d5c
Luke Harris <6074660+luk707@users.noreply.github.com>
2025-03-13 13:21:44 +0000

    Remove unused import

--- server/src/main.py
+++ server/src/main.py
@@ -17,10 +17,10 @@ from minio.error import S3Error
 
 from src.models.file import DirectoryListing
 from src.repositories.files.fastapi import FileRepositoryDependency
-from src.configuration import Configuration, ConfigurationDependency
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn.error")
+
 
 def get_logger():
     return logger
