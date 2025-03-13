2515dfa68a8521237281276b019e28de3fa1fb67
Luke Harris <6074660+luk707@users.noreply.github.com>
2025-03-13 21:32:32 +0000

    Add verify_basic_credentials abstract method to UserRepository

--- server/src/repositories/users/base.py
+++ server/src/repositories/users/base.py
@@ -8,3 +8,6 @@ class UserRepository(ABC):
 
     @abstractmethod
     async def get_user_avatar(id: str) -> tuple[bytes, str]: ...
+
+    @abstractmethod
+    async def verify_basic_credentials(username: str, password: str) -> bool: ...
